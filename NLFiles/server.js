d793748abebd6bf2231a1520627486fac75f39b3
patwyzlic <patrickwyzlic@gmail.com>
2022-06-12 19:34:58 -0400

    Middleware functional

--- NLFiles/server.js
+++ NLFiles/server.js
@@ -1,7 +1,18 @@
-//Import dependancies
+//Import dependencies
+require('dotenv').config()
+const express = require('express')
 
-//Include requred routes 
+//Include required routes 
+const middleware = require('./utils/middleware')
+
+//Create app
+const app = require('liquid-express-views')(express())
 
 //Include middleware
+middleware(app)
 
 //Listen to the server
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`app is listening to port ${PORT}`)
+})
